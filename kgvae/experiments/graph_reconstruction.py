c89e1795d12cb6e2767b8d113d518500c2fa8dd3
Thiviyan Thanapalasingam <thiviyan.t@gmail.com>
2025-09-11 17:50:50 +0200

    Clean up comments

--- kgvae/experiments/graph_reconstruction.py
+++ kgvae/experiments/graph_reconstruction.py
@@ -1,12 +1,4 @@
-"""
-Graph reconstruction experiments for Knowledge Graph VAE models.
-
-We will evaluate the model's ability to accurately reconstruct input graphs
-from their latent representations, measuring fidelity and information preservation.
-"""
-
-
-import argparse
+ aimport argparse
 import os
 import yaml
 import torch
